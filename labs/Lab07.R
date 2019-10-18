82edbbab3c8c6f392873f4beff28a0579c3ca94b
datageneration <datageneration1@gmail.com>
2019-10-18 09:38:11 -0500

    Add files via upload

---
+++ labs/Lab07.R
@@ -0,0 +1,22 @@
+# R Programming Lab (Linear Discrimination Analysis)
+# Adapted from ISLR Chapter 4 Lab
+
+require(ISLR)
+require(MASS)
+require(desc)
+attach(Smarket)
+## Linear Discriminant Analysis
+freq(Direction)
+lda.fit=lda(Direction~Lag1+Lag2,data=Smarket, subset=Year<2005)
+lda.fit
+plot(lda.fit, col="dodgerblue")
+Smarket.2005=subset(Smarket,Year==2005)
+lda.pred=predict(lda.fit,Smarket.2005)
+names(lda.pred)
+lda.class=lda.pred$class
+table(lda.class,Direction.2005)
+data.frame(lda.pred)[1:5,]
+table(lda.pred$class,Smarket.2005$Direction)
+mean(lda.pred$class==Smarket.2005$Direction)
+
+
